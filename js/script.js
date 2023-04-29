f7d7eb4d76efc417d4f192dfd8f89deaccc90979
lsavoie <twosavoie@gmail.com>
2023-04-29 00:41:41 -0500

    Added toggle to show and hide image. Updated styling

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
 const showCatButton = document.querySelector(".show-cat");
-const catImage = document.querySelector(".cat");
+const catImage = document.querySelector("figure.cat");
+// console.log(catImage)
 
-showCatButton.addEventListener("click", function()  { catImage.classList.add("show");
-});
+// Event listener just shows cat image. 
+// showCatButton.addEventListener("click", function()  { catImage.classList.add("show");
+// });
+
+showCatButton.addEventListener("click", function(){
+  if (catImage.classList.contains("show")) {
+    catImage.classList.remove("show")
+    showCatButton.innerText = "Wait, come back!"
+    showCatButton.classList.add("disappear")
+  } else {
+    showCatButton.classList.remove("disappear")
+    showCatButton.innerText = "Shoo, cat"
+    catImage.classList.add("show")
+  }
+})
+
 
 // Add?
-// image shows and disappears
+// done - image shows and disappears
 // change CSS
 // separate the listener and handler into different functions
